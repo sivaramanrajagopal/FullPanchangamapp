1a42ef7e22c489209b0ac6f6a4daa3c62c5b4e4d
SIVARAMAN RAJAGOPAL <82695306+sivaramanrajagopal@users.noreply.github.com>
2025-05-21 11:28:01 +0000

    Add next.config.js to ignore ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Other Next.js configuration options
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
